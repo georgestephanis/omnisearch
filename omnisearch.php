3eab5124019347cdfc2857d766b0188b5a2fe625
George Stephanis <george@stephanis.info>
2013-08-28 18:56:54 -0400

    Change the plugin description to note its feature pitch for core.

--- omnisearch.php
+++ omnisearch.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Omnisearch
 Plugin URI: https://github.com/georgestephanis/omnisearch
-Description: Enables an Omnisearch in WordPress.
+Description: Enables an Omnisearch in WordPress. Feature pitch for Core in 3.8.
 Author: George Stephanis
 Version: 1.0
 Author URI: http://stephanis.info/
